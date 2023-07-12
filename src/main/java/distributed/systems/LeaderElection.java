9aa06a0d28680dd7dbed758451b0f512f06e7059
RafaelAPNascimento <rfx.go.on@gmail.com>
2023-07-12 15:09:04 -0300

    maven assembly

--- src/main/java/distributed/systems/LeaderElection.java
+++ src/main/java/distributed/systems/LeaderElection.java
@@ -1,12 +1,9 @@
 package distributed.systems;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -18,21 +15,46 @@ public class LeaderElection implements Watcher {
     private static final int SESSION_TIMEOUT = 3000;
     private static final String ELECTION_NAMESPACE = "/election";
 
+    private String currentZnodeName;
+
     private ZooKeeper zooKeeper;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
 
         LeaderElection leaderElection = new LeaderElection();
 
         leaderElection.connectToZookeeper();
+
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
+
         leaderElection.run();
         leaderElection.close();
 
         LOGGER.info("Disconnected from Zookeeper, exiting application");
     }
 
-    public void volunteerForLeadership() {
+    public void volunteerForLeadership() throws InterruptedException, KeeperException {
 
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        LOGGER.info("znode name " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void electLeader() throws InterruptedException, KeeperException {
+
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+        Collections.sort(children);
+
+        String smallestChildren = children.get(0);
+
+        if (smallestChildren.equals(currentZnodeName)) {
+            LOGGER.info("I am the leader");
+            return;
+        }
+        LOGGER.info("I am note the leader, " + smallestChildren + " is the leader");
     }
 
     private void run() throws InterruptedException {
